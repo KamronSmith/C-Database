afa476542383fe2c2f5303e1b96558f05af53b69
Kamron Smith <kamrosmith@gmail.com>
2025-10-26 12:20:13 -0700

    Implemented opening and reading the database file

--- src/main.c
+++ src/main.c
@@ -7,14 +7,18 @@
 #include "file.h"
 #include "parse.h"
 
-void print_usage(char *nargv[]) {
-
+void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n  - create new database file\n");
+  printf("\t -f  - (required) path to database file");
 }
 
 int main(int argc, char *argv[]) { 
   int c;
   bool newfile = false;
   char *filepath = NULL;
+
+  int database_file_descriptor = -1;
 
   while ((c = getopt(argc, argv, "nf:")) != -1) {
     switch (c) {
@@ -35,6 +39,24 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  printf("Newfile: %d\n", newfile);
-  printf("Filepath: %s", filepath);
+  if (filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+
+    return 0;
+  }
+
+  if (newfile) {
+    database_file_descriptor = create_db_file(filepath);
+    if (database_file_descriptor == STATUS_ERROR) {
+      printf("Unable to create database file\n");
+      return -1;
+    }
+  } else {
+    database_file_descriptor = open_db_file(filepath);
+    if (database_file_descriptor == STATUS_ERROR) {
+      printf("Unable to open database file");
+      return -1;
+    }
+  }
 }
